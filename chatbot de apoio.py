740f912f9cbb6e31c0d5186c182aaf7a07f4741d
Matheus-dev <matheusaugusto2029@gmail.com>
2026-03-20 10:46:37 -0300

    Add files via upload
    um  simples chatbot de apoio, feito para auxiliar as pessoas de acordo com seus sentimentos.

---
+++ chatbot de apoio.py
@@ -0,0 +1,25 @@
+print ("olá sou um chatbot de apoio 🤖")
+nome = input("digite seu nome para continuarmos:")
+print (f"prazer, {nome}!" )
+print (f"esse chat não substitui ajuda profissional ⚠")
+sentimentos = input ("como está se sentindo hoje?")
+if "alegre" in sentimentos:
+    print("que otimo que esteja feliz😊, espero que continue assim! sabia que a felicidade é o segredo do sucesso? quando se vive de uma forma mais alegre você pode até viver mais. olha só isso não seria otimo?")
+    print("todos os direitos reservados desse chat bot pertence ao estudante de ciências da computação, matheus augusto")
+elif "ansioso"in sentimentos:
+ print ("sinto muito que esteja se sentindo assim, uma dica que posso te dar é descansar, ouvir musica, beber agua, sair na rua e fazer coisas que goste. mas lembre-se, sempre tenha alguem de confiança para desabafar seus sentimentos! ")
+ print("todos os direitos reservados desse chat bot pertence ao estudante de ciências da computação, matheus augusto")
+elif "triste" in sentimentos:
+   print("realmente estar triste é algo muito ruim, lamento pelo que esteja passando. mas as coisas vão melhorar tenha certeza disso. observação: se sua tristeza persistir por dias e até meses recomendo ir ao médico, pois pode ser tratar de uma depressão. lembrando sou programado para te auxiliar e dar apoio ao seus sentimentos, por isso não descarte a opção de um profissional da aréa de pscicologia 😉.")
+   print("todos os direitos reservados desse chat bot pertence ao estudante de ciências da computação, matheus augusto")
+elif "raiva" in sentimentos:
+   print("cuidado com a raiva! esse é um sentimento muito perigoso, tanto para a saude quanto a tomada de decisões preciptadas que podem afetar a outros. entendo que nesse momento esteja com raiva, e como um bot de apoio quero te ajudar a se livrar dessa raiva. uma coisa que sempre ajuda é contar até dez; sim esse simples ato pode relaxar sua mente e seus músculos te deixando em estado mais relaxado e calmo, ajudando você a tomar decisões de uma forma mais racional. ")
+   print("todos os direitos reservados desse chat bot pertence ao estudante de ciências da computação, matheus augusto")
+elif "medo" in sentimentos:
+ print ("entendo que esteja com medo, essa é uma reação que faz o cérebro liberar diversas reações quimicas, tais como: adrenalina, cortisol e neurotransmissores de alerta, mas tirando essa parte de ciências, entendo que esteja com medo, mas relaxe daqui a pouco esse medo passa num piscar de olhos. recomendo que tente se acalmar respirando fundo e não confrontando seu medo se não estiver pronto para enfrenta-lo. ")
+ print("todos os direitos reservados desse chat bot pertence ao estudante de ciências da computação, matheus augusto")
+elif "depressivo" in sentimentos:
+  print("sua situação é bastante preocupante,você chegou no apice da tristeza profunda, você não esta sozinho tem familia, amigos e quem sabe um animal de estimação em sua casa; se você se sente sozinho ligue para o número 188, esse numero é do centro de valorização da vida onde eles podem de auxilar por 24 horas por dia, lembre-se que tudo é passageiro, talvez o emprego que tenha perdido mais pra frente apareça outro melhor e isso serve para qualquer outra coisa. e se houver perda de um animal e parentes honre a memoria deles, até porque eles não queriam te ver assim, tenha certeza disso.")
+  print("todos os direitos reservados desse chat bot pertence ao estudante de ciências da computação, matheus augusto")
+else:
+  print("já que não esta sentindo nada, não tem como eu te auxiliar")
