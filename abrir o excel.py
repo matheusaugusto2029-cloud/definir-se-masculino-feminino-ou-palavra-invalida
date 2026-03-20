b3049241acaeefa282fb2e50a95f0c74a2c0b393
Matheus-dev <matheusaugusto2029@gmail.com>
2026-03-20 14:45:19 -0300

    Add files via upload
    Automação para abrir o Excel com Pyautogui

---
+++ abrir o excel.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+pyautogui.PAUSE = 3
+pyautogui. press ("win")
+pyautogui.write ("excel")
+pyautogui.press("enter")
+pyautogui.moveTo (x=76, y=236)
+pyautogui.press("enter")
+pyautogui.moveTo (x=57, y=233)
