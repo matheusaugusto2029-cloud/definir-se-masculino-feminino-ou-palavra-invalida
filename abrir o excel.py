e3789bbc7b82551779ff39b8d301717c6720d06d
Matheus-dev <matheusaugusto2029@gmail.com>
2026-09-25 11:13:17 -0300

    Delete abrir o excel.py

--- abrir o excel.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import time
-pyautogui.PAUSE = 3
-pyautogui. press ("win")
-pyautogui.write ("excel")
-pyautogui.press("enter")
-pyautogui.moveTo (x=76, y=236)
-pyautogui.press("enter")
-pyautogui.moveTo (x=57, y=233)
